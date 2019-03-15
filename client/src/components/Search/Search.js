a72bd96f231136228ea675486b01d6d06aeff4f5
Faheem <faheemtech1@gmail.com>
2019-03-15 12:27:32 +0530

    search component added

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,12 +1,36 @@
 import React from "react";
 
+import "./Search.css";
+
 const search = () => {
   return (
     <div className="container">
-      <p className="h3 text-danger">
-        We are the group that handles the save4thpillar. <br />
-        you are in search
-      </p>
+      <div className="row justify-content-md-center">
+        <div className="col-lg-10 col-md-12 col-sm-12">
+          <form className="navbar-form searchbar" action="">
+            <div className="form-group" style={{ display: "inline" }}>
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search News Channels, Anchors, Journalists and more..."
+                />
+                <button type="submit">
+                  <span className="input-group-addon">
+                    <i
+                      className="fa fa-search"
+                      style={{
+                        color: "white"
+                      }}
+                    />
+                  </span>
+                </button>
+              </div>
+              <hr color="grey" style={{ marginTop: "1px" }} />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
