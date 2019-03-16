e03935829308cde89b05d96e41ce3337e2809c2d
Faheem <faheemtech1@gmail.com>
2019-03-16 23:42:01 +0530

    about has more content

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -5,11 +5,17 @@ const about = () => {
     <div className="container">
       <p
         className="h4 text-danger"
-        style={{ lineHeight: "50px", marginTop: "25%" }}
+        style={{ lineHeight: "40px", marginTop: "25%" }}
       >
         We, here at save4thpillar are group of people who are trying to hard to
         make this world a better place. The name as suggessts that Media is our
-        fourth pillar of any Democracy.
+        fourth pillar of any Democracy. So, we are trying hard to make a
+        transparent open voting system. <br /> Basically save4thpillar is open
+        to all platform where everyone can come and vote for their favourite
+        News Channels, News Anchors, Jounalists etc.
+        <br />
+        For controlling the dublicacy we are trying to make a high filtering and
+        IP aunthentication.
       </p>
     </div>
   );
