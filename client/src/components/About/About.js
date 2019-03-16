8909e4092cc2b5662385a158e1d0ffe96ce43985
Faheem <faheemtech1@gmail.com>
2019-03-16 23:33:00 +0530

    About has info

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -4,7 +4,9 @@ const about = () => {
   return (
     <div className="container">
       <p className="h3 text-danger">
-        We are the group that handles the save4thpillar.
+        We, here at save4thpillar are group of people who are trying to hard to
+        make this world a better place. The name as suggessts that Media is our
+        fourth pillar of any Democracy.
       </p>
     </div>
   );
