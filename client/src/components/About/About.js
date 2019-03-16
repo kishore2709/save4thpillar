acdf1a4dfcd0eb6851f59b21ee32964e41683ee9
Faheem <faheemtech1@gmail.com>
2019-03-16 23:35:31 +0530

    about details has been in middle

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -3,7 +3,10 @@ import React from "react";
 const about = () => {
   return (
     <div className="container">
-      <p className="h3 text-danger">
+      <p
+        className="h4 text-danger"
+        style={{ lineHeight: "50px", marginTop: "25%" }}
+      >
         We, here at save4thpillar are group of people who are trying to hard to
         make this world a better place. The name as suggessts that Media is our
         fourth pillar of any Democracy.
