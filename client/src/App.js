200b2cb0f28660c27b558a9af3f9e4dc13344207
Faheem <faheemtech1@gmail.com>
2019-03-17 23:54:08 +0530

    fetching the / res to react frontend

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,29 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import axios from "axios";
+
 import "./App.css";
 
 class App extends Component {
+  state = {
+    name: ""
+  };
+
+  componentDidMount() {
+    axios
+      .get("/", (req, res) => {})
+      .then((req, res) => {
+        console.log(res);
+        this.setState({ name: res });
+      });
+  }
+
   render() {
     return (
       <Router>
         <div className="App">
+          <div>{this.state.name}</div>
           <Navbar />
         </div>
       </Router>
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Welcome to Login page");
+  res.send("Welcome to home page");
 });
 
 module.exports = router;
