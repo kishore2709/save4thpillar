f33832ec581c57bb8e9ebaf079c9ac4eac9d1002
Faheem <faheemtech1@gmail.com>
2019-03-17 23:32:45 +0530

    API is connected to localhost

--- server.js
+++ server.js
@@ -5,21 +5,23 @@ const bodyParser = require("body-parser");
 const app = express();
 
 // BodyParser Middleware
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // DB config
-const db = require("./config/keys").mongoURI;
+// const db = require("./config/keys").mongoURI;
 
 // DB connect
 // { useNewUrlParser: true } added so i can add '$' in my pass character
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => {
-    console.log("database connected");
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(db, { useNewUrlParser: true })
+//   .then(() => {
+//     console.log("database connected");
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+app.use("/", require("./routes/index"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server running at ${port}`));
