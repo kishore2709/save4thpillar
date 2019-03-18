2850ebd46623d75a12612c396b542927e7284dae
Faheem <faheemtech1@gmail.com>
2019-03-18 23:07:29 +0530

    flash is set to show the message when user registerd or any error

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
 // BodyParser Middleware
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
 
 // DB config
 const db = require("./config/keys").mongoURI;
