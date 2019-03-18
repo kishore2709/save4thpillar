aff0284d99933db672de1ae88bed27fd1da91d2b
Faheem <faheemtech1@gmail.com>
2019-03-18 23:53:39 +0530

    ensure login is added and dashboard  route is protected

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
 const router = express.Router();
+const { ensureAuthenticated } = require("../config/auth");
 
 // Home page
 router.get("/", (req, res) => {
@@ -8,6 +8,8 @@ router.get("/", (req, res) => {
 });
 
 // Dashboard
-router.get("/dashboard", (req, res) => res.render("dashboard"));
+router.get("/dashboard", ensureAuthenticated, (req, res) =>
+  res.render("dashboard")
+);
 
 module.exports = router;
