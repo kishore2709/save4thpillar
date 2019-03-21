81b5245aeeb43f47fff53fd00eb123e19c68c21c
Faheem <faheemtech1@gmail.com>
2019-03-21 21:59:45 +0530

    authReducer added, rootreducer is added in store

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 
+import store from "./store";
 import Navbar from "./components/Layout/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
-
-const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   state = {
