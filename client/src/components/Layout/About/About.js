01238c09882ed4d4d845129856eeafaf51880a14
Faheem <faheemtech1@gmail.com>
2019-03-24 22:12:37 +0530

    about part has overview

--- client/src/components/Layout/About/About.js
+++ client/src/components/Layout/About/About.js
@@ -14,35 +14,39 @@ class About extends Component {
           </div>
         </div>
         <div className="container">
-          <p
-            className="h4 text-danger"
-            style={{ lineHeight: "40px", marginTop: "25%" }}
-          >
-            We, here at save4thpillar are group of people who are trying to hard
-            to make this world a better place. The name as suggessts that Media
-            is our fourth pillar of any Democracy. So, we are trying hard to
-            make a transparent open voting system. <br /> Basically
-            save4thpillar is open to all platform where everyone can come and
-            vote for their favourite News Channels, News Anchors, Jounalists
-            etc.
-            <br />
-            For controlling the dublicacy we are trying to make a high filtering
-            and IP aunthentication.
-            <br />
-            <br />
-            <br />
-            We, here at save4thpillar are group of people who are trying to hard
-            to make this world a better place. The name as suggessts that Media
-            is our fourth pillar of any Democracy. So, we are trying hard to
-            make a transparent open voting system. <br /> Basically
-            save4thpillar is open to all platform where everyone can come and
-            vote for their favourite News Channels, News Anchors, Jounalists
-            etc.
-            <br />
-            For controlling the dublicacy we are trying to make a high filtering
-            and IP aunthentication.
-          </p>
+          <div className="row justify-content-center">
+            <div className="col">
+              <p className="h2 text-left subhead">
+                At save4thpillar.com, people come and vote for journalists as
+                per the news they are providing
+              </p>
+            </div>
+
+            <div
+              className="row justify-content-center"
+              style={{ marginTop: "40px" }}
+            >
+              <div className="col">
+                <p
+                  className="h4 text-left"
+                  style={{
+                    lineHeight: "1.408"
+                  }}
+                >
+                  We, here at save4thpillar are group of people who are trying
+                  hard to make this world a better place. The name as suggessts
+                  that Media is our fourth pillar of any Democracy. So, we are
+                  trying hard to make a transparent open voting system. <br />{" "}
+                  Basically save4thpillar is open to all platform where everyone
+                  can come and vote for their favourite News Channels, News
+                  Anchors, Jounalists etc. For controlling the dublicacy we are
+                  trying to make a high filtering and IP aunthentication.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+        <p style={{ marginTop: "500px" }}>Footer goes here</p>
       </div>
     );
   }
