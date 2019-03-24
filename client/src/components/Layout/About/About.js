df78c9d56256d3d129adf6f1b33dcf817aaf98df
Faheem <faheemtech1@gmail.com>
2019-03-24 21:35:58 +0530

    about head image and position is responsive for sm to lg

--- client/src/components/Layout/About/About.js
+++ client/src/components/Layout/About/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 
-// import AboutHead from "../../../assets/about/about.jpg";
 import "./About.css";
 
 class About extends Component {
@@ -10,7 +9,7 @@ class About extends Component {
         <div className="background">
           <div className="transbox">
             <div>
-              <p className="h2">The world's platform for vote</p>
+              <p className="h1">The world's platform for vote</p>
             </div>
           </div>
         </div>
@@ -19,6 +18,19 @@ class About extends Component {
             className="h4 text-danger"
             style={{ lineHeight: "40px", marginTop: "25%" }}
           >
+            We, here at save4thpillar are group of people who are trying to hard
+            to make this world a better place. The name as suggessts that Media
+            is our fourth pillar of any Democracy. So, we are trying hard to
+            make a transparent open voting system. <br /> Basically
+            save4thpillar is open to all platform where everyone can come and
+            vote for their favourite News Channels, News Anchors, Jounalists
+            etc.
+            <br />
+            For controlling the dublicacy we are trying to make a high filtering
+            and IP aunthentication.
+            <br />
+            <br />
+            <br />
             We, here at save4thpillar are group of people who are trying to hard
             to make this world a better place. The name as suggessts that Media
             is our fourth pillar of any Democracy. So, we are trying hard to
