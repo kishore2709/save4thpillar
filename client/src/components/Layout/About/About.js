d17ef16f1ebdd5e097c2d82c3d701dbc0780edd8
Faheem <faheemtech1@gmail.com>
2019-03-24 22:54:40 +0530

    who uses has 2 content and icons

--- client/src/components/Layout/About/About.js
+++ client/src/components/Layout/About/About.js
@@ -5,7 +5,7 @@ import "./About.css";
 class About extends Component {
   render() {
     return (
-      <div>
+      <div className="bg-light">
         <div className="background">
           <div className="transbox">
             <div>
@@ -13,9 +13,9 @@ class About extends Component {
             </div>
           </div>
         </div>
-        <div className="container">
+        <div className="container ">
           <div className="row justify-content-center">
-            <div className="col">
+            <div className="col-auto col-sm-11">
               <p className="h2 text-left subhead">
                 At save4thpillar.com, people come and vote for journalists as
                 per the news they are providing
@@ -26,7 +26,7 @@ class About extends Component {
               className="row justify-content-center"
               style={{ marginTop: "40px" }}
             >
-              <div className="col">
+              <div className="col-auto col-md-11 col-sm-11">
                 <p
                   className="h4 text-left"
                   style={{
@@ -42,6 +42,54 @@ class About extends Component {
                   Anchors, Jounalists etc. For controlling the dublicacy we are
                   trying to make a high filtering and IP aunthentication.
                 </p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="row justify-content-around"
+            style={{ marginTop: "100px" }}
+          >
+            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-left">
+              <div className="row">
+                <div className="col-4">
+                  <i
+                    className="fa fa-users fa-4x text-danger "
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="col-8 col-sm-8 text-left ">
+                  <div className="row">
+                    <h4>Open to everyone</h4>
+                  </div>
+                  <div className="row">
+                    <p>
+                      Open to everyone means anyone can come and vote. Becasue
+                      it is everyone's right. You can also express your opinion.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-left">
+              <div className="row">
+                <div className=" col-4">
+                  <i
+                    className="fa fa-balance-scale fa-4x text-danger "
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="col-lg-8 col-md-8 col-sm-8 text-left">
+                  <div className="row">
+                    <h4>Government</h4>
+                  </div>
+                  <div className="row">
+                    <p>
+                      Open to everyone means anyone can come and vote. Becasue
+                      it is everyone's right. You can also express your opinion.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
