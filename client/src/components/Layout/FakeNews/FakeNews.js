53a407fc2e47a644e51eed57c4d59153f33d5c2f
Faheem <faheemtech1@gmail.com>
2019-03-26 00:27:08 +0530

    sidebar headings has been added

--- client/src/components/Layout/FakeNews/FakeNews.js
+++ client/src/components/Layout/FakeNews/FakeNews.js
@@ -6,15 +6,13 @@ const fakeNews = () => {
     <div>
       <div className="page-sidebar expandit text-left">
         <div className="sidebar-inner" id="main-menu-wrapper">
-          <ul className="wraplist" style={{ height: "auto" }}>
-            <li className="menusection">Main</li>
-
+          <ul className="wraplist mt-5 ml-3">
             <li>
               <a href="/">
                 <span className="sidebar-icon">
-                  <i className="fa fa-dashboard" />
+                  <i className="fa fa-home" />
                 </span>{" "}
-                <span className="menu-title">Dashboard</span>
+                <span className="menu-title">Home</span>
               </a>
             </li>
             <li>
@@ -22,39 +20,48 @@ const fakeNews = () => {
                 <span className="sidebar-icon">
                   <i className="fa fa-bullseye" />
                 </span>{" "}
-                <span className="menu-title">Goal Management</span>
+                <span className="menu-title">Trending</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="/">
+                <span className="sidebar-icon">
+                  <i className="fa fa-video" />
+                </span>{" "}
+                <span className="menu-title">Video </span>
               </a>
             </li>
             <li>
               <a href="/">
                 <span className="sidebar-icon">
-                  <i className="fa fa-users" />
+                  <i className="fa fa-book" />
                 </span>{" "}
-                <span className="menu-title">Manage Users</span>
+                <span className="menu-title">Article</span>
               </a>
             </li>
             <li>
               <a href="/">
                 <span className="sidebar-icon">
-                  <i className="fa fa-calendar" />
+                  <i className="fa fa-user" />
                 </span>{" "}
-                <span className="menu-title">Leaderboard</span>
+                <span className="menu-title">you rated</span>
               </a>
             </li>
             <li>
               <a href="/">
                 <span className="sidebar-icon">
-                  <i className="fa fa-suitcase" />
+                  <i className="fa fa-question-circle" />
                 </span>{" "}
-                <span className="menu-title">Meetings</span>
+                <span className="menu-title">help</span>
               </a>
             </li>
             <li>
               <a href="/">
                 <span className="sidebar-icon">
-                  <i className="fa fa-lock" />
+                  <i className="fa fa-comments" aria-hidden="true" />
                 </span>{" "}
-                <span className="menu-title">Sign Out</span>
+                <span className="menu-title">send us feedback</span>
               </a>
             </li>
           </ul>
