5d59a1a4746ae6bf3df6a4c406d2b3a3781c5f75
Faheem <faheemtech1@gmail.com>
2019-03-29 12:37:07 +0530

    search issue fixed

--- client/src/components/Layout/Search/Search.js
+++ client/src/components/Layout/Search/Search.js
@@ -5,31 +5,33 @@ import Footer from "../Footer/Footer";
 
 const search = () => {
   return (
-    <div className="container">
-      <div className="row justify-content-md-center">
-        <div className="col-lg-10 col-md-12 col-sm-12">
-          <form className="navbar-form searchbar" action="">
-            <div className="form-group" style={{ display: "inline" }}>
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search News Channels, Anchors, Journalists and more..."
-                />
-                <button type="submit">
-                  <span className="input-group-addon">
-                    <i
-                      className="fa fa-search"
-                      style={{
-                        color: "white"
-                      }}
-                    />
-                  </span>
-                </button>
+    <div>
+      <div className="container">
+        <div className="row justify-content-md-center ">
+          <div className="col-lg-10 col-md-12 col-sm-12">
+            <form className="navbar-form searchbar" action="">
+              <div className="form-group" style={{ display: "inline" }}>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search News Channels, Anchors, Journalists and more..."
+                  />
+                  <button type="submit">
+                    <span className="input-group-addon">
+                      <i
+                        className="fa fa-search"
+                        style={{
+                          color: "white"
+                        }}
+                      />
+                    </span>
+                  </button>
+                </div>
+                <hr color="grey" style={{ marginTop: "1px" }} />
               </div>
-              <hr color="grey" style={{ marginTop: "1px" }} />
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
       <Footer style={{ marginTop: "200px" }} />
