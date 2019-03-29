ea07e47bf960c0b9f3950aa7e9387635ca4f1946
Faheem <faheemtech1@gmail.com>
2019-03-29 12:19:39 +0530

    footer has content next is provide links

--- client/src/components/Layout/Footer/Footer.js
+++ client/src/components/Layout/Footer/Footer.js
@@ -4,8 +4,58 @@ import "./Footer.css";
 
 const footer = () => {
   return (
-    <div style={{ marginTop: "500px" }}>
-      <footer>footer goes here</footer>
+    <div
+      className="bg-dark text-light"
+      style={{
+        marginTop: "500px",
+        paddingTop: "40px",
+        paddingBottom: "40px"
+      }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3">
+            <p className="h6 text-uppercase pb-2">Company</p>
+
+            <p> About</p>
+            <p> Fake News</p>
+
+            <p>Career</p>
+            <p>Team</p>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3">
+            <p className="h6 text-uppercase pb-2">Support</p>
+            <p> Help </p>
+            <p> Guides</p>
+            <p> Contact us</p>
+            <p> privacy & Policies</p>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3">
+            <p className="h6 text-uppercase pb-2">Social</p>
+            <p>
+              <i className="fa fa-twitter" />
+            </p>
+            <p>
+              <i className="fa fa-facebook" />
+            </p>
+            <p>
+              <i className="fa fa-envelope" />
+            </p>
+            <p>
+              <i className="fa fa-linkedin" />
+            </p>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3">
+            <p className="h6 text-uppercase pb-2">Company</p>
+            <p> About</p>
+            <p> Fake News</p>
+            <p> Contact us</p>
+            <p> Register</p>
+            <p> Login</p>
+          </div>
+        </div>
+        <div className="row" />
+      </div>
     </div>
   );
 };
