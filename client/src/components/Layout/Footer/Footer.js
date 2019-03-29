b048f3352dc5efac1e6f3cdf273ab835b41c5fc4
Faheem <faheemtech1@gmail.com>
2019-03-29 12:34:18 +0530

    footer finished

--- client/src/components/Layout/Footer/Footer.js
+++ client/src/components/Layout/Footer/Footer.js
@@ -8,13 +8,12 @@ const footer = () => {
       className="bg-dark text-light"
       style={{
         marginTop: "500px",
-        paddingTop: "40px",
-        paddingBottom: "40px"
+        paddingTop: "40px"
       }}
     >
       <div className="container">
         <div className="row">
-          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3">
+          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3 text-left">
             <p className="h6 text-uppercase pb-2">Company</p>
 
             <p> About</p>
@@ -23,38 +22,47 @@ const footer = () => {
             <p>Career</p>
             <p>Team</p>
           </div>
-          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3">
+          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3 text-left">
             <p className="h6 text-uppercase pb-2">Support</p>
             <p> Help </p>
             <p> Guides</p>
             <p> Contact us</p>
             <p> privacy & Policies</p>
           </div>
-          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3">
+          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3 text-left">
             <p className="h6 text-uppercase pb-2">Social</p>
             <p>
-              <i className="fa fa-twitter" />
+              <i className="fa fa-twitter fa-lg" />
             </p>
             <p>
-              <i className="fa fa-facebook" />
+              <i className="fa fa-facebook fa-lg" />
             </p>
             <p>
-              <i className="fa fa-envelope" />
+              <i className="fa fa-envelope fa-lg" />
             </p>
             <p>
-              <i className="fa fa-linkedin" />
+              <i className="fa fa-linkedin fa-lg" />
             </p>
           </div>
-          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3">
-            <p className="h6 text-uppercase pb-2">Company</p>
-            <p> About</p>
-            <p> Fake News</p>
-            <p> Contact us</p>
-            <p> Register</p>
-            <p> Login</p>
+          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-3 text-left">
+            <p className="h6 text-uppercase pb-2">Featured</p>
+            <p> Press</p>
+            <p> News Channel</p>
+            <p> Magzine</p>
+            <p> Blog & Posts</p>
           </div>
         </div>
-        <div className="row" />
+        <hr />
+        <div className="row">
+          <div className="col text-center">
+            <footer class="page-footer font-small blue">
+              <div class="footer-copyright text-center mt-2 mb-3">
+                © 2019 Copyright:
+                <a href="https://www.save4thpillar.com"> save4thpillar.com</a>
+              </div>
+            </footer>
+          </div>
+        </div>
       </div>
     </div>
   );
