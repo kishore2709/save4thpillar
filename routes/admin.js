c65a2811ae31e8ddbcf14d68ea3643c84cfb7b04
Faheem <faheemtech1@gmail.com>
2019-04-04 20:32:30 +0530

    reducer is not working for add channel i.e. error is not caught

--- routes/admin.js
+++ routes/admin.js
@@ -120,11 +120,11 @@ router.post(
 
     // cheking validation
     if (!isValid) {
+      console.log(errors);
       return res.status(400).json(errors);
     }
 
     const { name, info, website, twitter, facebook } = req.body;
-    console.log(name, info, twitter, website, twitter, facebook);
 
     Channel.findOne({ name: name }).then(channel => {
       if (channel) {
--- routes/users.js
+++ routes/users.js
@@ -73,6 +73,7 @@ router.post("/login", (req, res) => {
 
   // cheking validation
   if (!isValid) {
+    console.log(errors);
     return res.status(400).json(errors);
   }
 
@@ -110,6 +111,7 @@ router.post("/login", (req, res) => {
         );
       } else {
         errors.password = "Password incorrect";
+        
         return res.status(400).json(errors);
       }
     });
