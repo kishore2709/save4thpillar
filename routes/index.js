cdf9939fa5700b0ffd13317c9dce0c7672e46336
FaheemTech <faheemtech1@gmail.com>
2019-04-11 00:15:46 +0530

    testing two accounts

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ const { ensureAuthenticated } = require("../config/auth");
 
 // Home page
 router.get("/", (req, res) => {
-  res.send("Welcome to home page");
+    res.send("Welcome to home page! testing ");
 });
 
 // Dashboard
 router.get("/dashboard", ensureAuthenticated, (req, res) =>
-  res.render("dashboard", {
-    name: req.user.name
-  })
+    res.render("dashboard", {
+        name: req.user.name
+    })
 );
 
 module.exports = router;
